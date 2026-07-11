33bcb655f9f5d3b13333169405e0943d3d80f657
jjmm-del <jimin8768@naver.com>
2026-07-11 19:14:20 +0900

    feat: AppointmentUI
    직책 리스트 Prefab으로 수정

--- MurimWorld/Assets/1.Scripts/UI/SubPanel/AppointmentSubPanelUI.cs
+++ MurimWorld/Assets/1.Scripts/UI/SubPanel/AppointmentSubPanelUI.cs
@@ -1,42 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using System;
+using System.Collections.Generic;
 
 public class AppointmentSubPanelUI : MonoBehaviour
 {
-    [Header("2단계: 직책 슬롯 버튼들")] //동적슬롯으로 변경
-    [SerializeField] private Button _leaderSlotButton;  //무림맹주
-    [SerializeField] private Button _strategistLotButton;   //책사
-    [SerializeField] private Button _medicineSlotButton;    //의약당주
-    [SerializeField] private Button _windSwordSlotButton;   //풍검대주
-    
-    [Header("2단계: 현재 임명된 자들의 이름 표시Text")]
-    [SerializeField] private TextMeshProUGUI _leaderNameText;
-    [SerializeField] private TextMeshProUGUI _strategistNameText;
-    [SerializeField] private TextMeshProUGUI _medicineNameText;
-    [SerializeField] private TextMeshProUGUI _windSwordNameText;
+    [Header("ScrollView Infra(동적슬롯 생성기")] //동적슬롯으로 변경
+    [SerializeField] private Transform _roleContentContainer;
+    [SerializeField] private GameObject _roleSlotPrefab;
     
     [Header("3단계: 최종 인재 명단 팝업 스크립트 참조")]
     [SerializeField] private CharacterListPopupUI _characterListPopupUI;
 
+    private List<RoleSlotItemUI> _spawnedSlots = new List<RoleSlotItemUI>();
     private void OnEnable()
     {
-        _leaderSlotButton.onClick.AddListener(() => OpenCharacterListPopup(RoleType.AllianceLeader));
-        _strategistLotButton.onClick.AddListener(() => OpenCharacterListPopup(RoleType.Strategist));
-        _medicineSlotButton.onClick.AddListener(() => OpenCharacterListPopup(RoleType.MedicineHead));
-        _windSwordSlotButton.onClick.AddListener(() => OpenCharacterListPopup(RoleType.WindSword));
-
-        UpdateCurrentRosterDisplay();
+        //창이 열릴 대 직책 슬롯들을 동적으로 생성
+        GenerateRoleSlots();
     }
 
-    private void OnDisable()
+    private void GenerateRoleSlots()
     {
-        _leaderSlotButton.onClick.RemoveAllListeners();
-        _strategistLotButton.onClick.RemoveAllListeners();
-        _medicineSlotButton.onClick.RemoveAllListeners();
-        _windSwordSlotButton.onClick.RemoveAllListeners();
-    }
+        foreach (Transform child in _roleContentContainer)
+        {
+            Destroy(child.gameObject);
+        }
 
+        _spawnedSlots.Clear();
+
+        foreach (RoleType role in Enum.GetValues(typeof(RoleType)))
+        {
+            if (role == RoleType.None) continue;
+            
+            GameObject go = Instantiate(_roleSlotPrefab, _roleContentContainer);
+            RoleSlotItemUI slotUI = go.GetComponent<RoleSlotItemUI>();
+
+            if (slotUI != null)
+            {
+                slotUI.Initialize(role, OpenCharacterListPopup);
+                _spawnedSlots.Add(slotUI);
+            }
+        }
+    }
+    
+    
     private void OpenCharacterListPopup(RoleType roleSlot)
     {
         if(_characterListPopupUI!=null)
@@ -47,24 +54,10 @@ public class AppointmentSubPanelUI : MonoBehaviour
 
     public void UpdateCurrentRosterDisplay()
     {
-        if (RosterManager.Instance == null) return;
-        _leaderNameText.text = "공석";
-        _strategistNameText.text = "공석";
-        _medicineNameText.text = "공석";
-        _windSwordNameText.text = "공석";
-
-        foreach (Character c in RosterManager.Instance.AllCharacters)
+        foreach (RoleSlotItemUI slot in _spawnedSlots)
         {
-            switch (c.CurrentRoleType)
-            {
-                case RoleType.AllianceLeader: _leaderNameText.text = c.BaseData.CharacterName; break;
-                case RoleType.Strategist:     _strategistNameText.text = c.BaseData.CharacterName; break;
-                case RoleType.MedicineHead:   _medicineNameText.text = c.BaseData.CharacterName; break;
-                case RoleType.WindSword:  _windSwordNameText.text = c.BaseData.CharacterName; break;
-            }
+            slot.RefreshAssignedCharacter();
         }
-        
-        
     }
     
 }
